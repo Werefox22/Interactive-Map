5b6b184fed31aa3a1eea22874f99c121f349a254
Werefox <jack@wrightus.com>
2022-04-09 09:50:16 -0400

    Began planning and linked css and javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+// First thing to do is get the user's location
+// Center the map on those coords, and drop a marker there as well
+
+// make a function to get the user's location
+
+// get the tiles for the map
+
+// get places from foursquare
+
+// drop a marker at the user's location
+
+// drop markers at different locations
+
+// group markers
+
+// add overlay selection
